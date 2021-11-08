36d0cd199bdb1b03268b8b42bc577b8008a7de95
Douglas Socorro <douglassocorro.work@gmail.com>
2021-11-08 09:15:57 -0400

    Fixed the port error

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 require('dotenv').config()
+const express = require('express')
 
-console.log(process.env)
+const APP = express()
+
+let { PORT } = process.env
+
+PORT = PORT || 3000
+
+APP.get('/', (req, res) => {
+
+	res.send(`<h1>Hola londoño</h1>\n${JSON.stringify(process.env, null, 4)}`)
+	res.end()
+})
+
+APP.listen(PORT, (err) => {
+	console.log(`Listing in: localhost:${PORT}`)
+})
